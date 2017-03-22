7b125f4edd0d02572a3ef7fa85a03d7d2e95d8f5
Ivan <r.rrivan@gmail.com>
2017-03-22 23:18:57 +0300

    mod

---
+++ backend/controllers/TreeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace backend\controllers;
+
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+
+class TreeController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
